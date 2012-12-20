4c7c94adeccd7cea4e84c47f34a2061c69fc82e3
James Freiwirth <james@ciseware.com>
2012-12-20 03:00:44 +0000

    Fixed brainfuck in firefox, updated default program, made console a bit nicer to use

--- Examples/Brainfuck/FunJS.Brainfuck.Logic/Program.fs
+++ Examples/Brainfuck/FunJS.Brainfuck.Logic/Program.fs
@@ -6,6 +6,7 @@ open System
 open FunJS.Core.Events
 
 type Dom = FunJS.TypeScript.Api< @"..\..\..\Examples\Typings\lib.d.ts" >
+type J = FunJS.TypeScript.Api< @"..\..\..\Examples\Typings\jquery.d.ts" >
 
 [<JSEmit("alert({0});")>]
 let alert (x:obj): unit = failwith "never"
@@ -13,6 +14,9 @@ let alert (x:obj): unit = failwith "never"
 // Manual overrides until the typescript type provider supports inheritance
 [<JSEmit("return {0}.value;")>]
 let getValue element = failwith "never"
+
+[<JSEmit("{0}.value = '';")>]
+let clear element : unit = failwith "never"  
 
 [<JSEmit("{0}.value += {1};")>]
 let output element value : unit = failwith "never"  
@@ -51,7 +55,7 @@ let private addEventListener (element:obj) (eventName:string) (handler:'T -> uni
 [<FunJS.JSEmit("{0}.removeEventListener({1},{2});")>]
 let private removeEventListener (element:obj) (eventName:string) (handler:'T -> unit) : unit = failwith "never"
 
-[<FunJS.JSEmit("return {0}.keyCode;")>]
+[<FunJS.JSEmit("return {0}.charCode;")>]
 let getKeyFromEvent e : byte = failwith "never"
 
 /// A wrapper around DOM Events
@@ -66,31 +70,48 @@ type DomEvent<'T> (element, eventName) =
       member this.AddHandler (f : 'T -> unit) = addEventListener element eventName f
       member this.RemoveHandler (f : 'T -> unit) = removeEventListener element eventName f
 
+[<JSEmit("return {0}.srcElement;")>]
+let src e : Dom.HTMLInputElement' = failwith "notimpl"
+
 let main() =
 
     let txtCode = Dom.document.getElementById("code")    
     let submit = Dom.document.getElementById("execute-code")
     let console = Dom.document.getElementById("console-text")
 
+    /// JQuery selector
+    let (!.) (str:string) = J.jQuery.Invoke(str)
+
+    // Wire up the virtual keyboard buttons
+    (!. ".keyboard-button").click(fun (e:J.JQueryEventObject') ->
+       let el = src e
+       output txtCode el.value) |> ignore
+
+    let waitingForInput = (!. "#waiting-for-input")
+
     let consoleKeyPressEvt = new DomEvent<Dom.KeyboardEvent'>(console, "keypress") :> IEvent<_>
 
-    let getNextChar =             
+    let getNextChar =                    
        Async.FromContinuations(fun(cont, error, cancelled) ->            
           let sub = ref Unchecked.defaultof<IDisposable>
           let nextChar (e:Dom.KeyboardEvent') = 
               (!sub).Dispose()
-              cont <| getKeyFromEvent e
               console.disabled <- true //TODO: Should be using readonly rather than disabled but type provider doesn't support inheritance
-          console.disabled <- false    //TODO: Should be using readonly rather than disabled but type provider doesn't support inheritance
-          let observer = ActionObserver<Dom.KeyboardEvent'>(nextChar, (fun e -> ()), (fun () -> ()))
-          sub := consoleKeyPressEvt.Subscribe(observer)) //TODO: Implement Observable.Take to avoid this mess
+              waitingForInput.fadeOut(30, fun _ ->
+                 cont <| getKeyFromEvent e) |> ignore              
+          waitingForInput.fadeIn(300, fun _ ->
+            console.disabled <- false    //TODO: Should be using readonly rather than disabled but type provider doesn't support inheritance          
+            console.focus()
+            let observer = ActionObserver<Dom.KeyboardEvent'>(nextChar, (fun e -> ()), (fun () -> ()))
+            sub := consoleKeyPressEvt.Subscribe(observer)) |> ignore)  //TODO: Implement Observable.Take to avoid this mess)
     
     let outputChar = console |> output
           
     let executeCode (e:Dom.MouseEvent') =        
+        clear console        
         let code = txtCode |> getValue
         run code getNextChar outputChar |> Async.StartImmediate
-        ignore |> box
+        ignore |> box        
 
     submit.onclick <- executeCode    
 
