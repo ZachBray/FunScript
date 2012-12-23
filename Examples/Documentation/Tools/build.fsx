7c5d7932ea417b1e5b9854d3046183a483f132a0
Tomas Petricek <tomas@tomasp.net>
2012-12-23 22:08:29 +0100

    Updating documentation and tools

--- Examples/Documentation/Tools/build.fsx
+++ Examples/Documentation/Tools/build.fsx
@@ -1,19 +1,36 @@
 ﻿// --------------------------------------------------------------------------------------
-// Builds the documentation from FSX files in the 'samples' directory
-// (the documentation is stored in the 'docs' directory)
+// Builds the documentation 
 // --------------------------------------------------------------------------------------
 
-#I "../packages/FSharp.Formatting.1.0.6/lib/net40"
-#load "../packages/FSharp.Formatting.1.0.6/literate/literate.fsx"
+#I "../../packages/FSharp.Formatting.1.0.6/lib/net40"
+#load "../../packages/FSharp.Formatting.1.0.6/literate/literate.fsx"
 open System.IO
 open FSharp.Literate
 
-let source = __SOURCE_DIRECTORY__
-let template = Path.Combine(source, "template.html")
-let tutorial = Path.Combine(source, "../Tutorial/Page.fs")
-let output = Path.Combine(source, "../Documentation/output/tutorial.html")
-let options = 
-  "-r:\"" + Path.Combine(source, "../../FunJS/bin/Debug/FunJS.dll") + "\" " +
-  "-r:\"" + Path.Combine(source, "../../FunJS.TypeScript/bin/Debug/FunJS.TypeScript.dll") + "\""
+let generateDocs () =
+  let extractString tag (page:string) = 
+    let startIndex = page.IndexOf("<!-- [" + tag + "] -->")
+    let endIndex = page.IndexOf("<!-- [/" + tag + "] -->")
+    page.Substring(startIndex, endIndex - startIndex)
 
-Literate.ProcessScriptFile(tutorial, template, output, compilerOptions = options)
+  let source = __SOURCE_DIRECTORY__
+  let template = Path.Combine(source, "template.html")
+  let tutorial = Path.Combine(source, "../../Tutorial/Page.fs")
+  let output = Path.Combine(source, "../Output/tutorial.html")
+  let options = 
+    "-r:\"" + Path.Combine(source, "../../../FunScript/bin/Debug/FunScript.dll") + "\" " +
+    "-r:\"" + Path.Combine(source, "../../../FunScript.TypeScript.Interop/bin/Debug/FunScript.TypeScript.Interop.dll") + "\" " +
+    "-r:\"" + Path.Combine(source, "../../../FunScript.TypeScript/bin/Debug/FunScript.TypeScript.dll") + "\""
+
+  let html = Path.Combine(source, "../../Tutorial/bin/Debug/index.html")
+  let page = File.ReadAllText(html)
+
+  let js = Path.Combine(source, "../../Tutorial/bin/Debug/tutorial.js")
+  let info = 
+    [ "sample-javascript", File.ReadAllText(js) 
+      "sample-body", extractString "body" page
+      "sample-title", extractString "title" page ]
+
+  Literate.ProcessScriptFile(tutorial, template, output, compilerOptions = options, replacements = info)
+
+generateDocs ()
--- Examples/Tutorial/Page.fs
+++ Examples/Tutorial/Page.fs
@@ -1,58 +1,64 @@
-﻿[<ReflectedDefinition>]
-module Program
+﻿(**
+# FunScript Tutorial
+
+This example shows basic features of FunScript. It demonstrates how to use the TypeScript
+type provider to access standard DOM library functions and to call JavaScript libraries 
+(such as JQuery). 
+
+## Project structure
+
+FunScript is a library that can be called in a number of ways. Most of the samples in the
+`Examples` directory follow the same pattern - they are executable programs that, when
+started, generate JavaScript source code (as `*.js` file) and run a simple HTTP web 
+server that can be used to view the generated web.
+
+In this mode, a typical project needs to reference `FunScript.dll` (containing the core
+JavaScript translator), `FunScript.TypeScript.dll` (a type provider that generates nice
+types for calling JavaScript libraries) and `FunScript.TypeScript.Interop.dll` (which 
+is required by the TypeScript type provider).
+
+All code that is translated to JavaScript needs to be marked with the `FunScript.JS`
+attribute (an alias for `ReflectedDefinition`):
+*)
+[<FunScript.JS>]
+module Page
 
 open FunScript
 open FunScript.TypeScript
-open System.Threading
+
+(**
+The `FunScript.TypeScript` namespace contains the TypeScript provider, which is 
+discussed in the next section.
+
+## Using the TypeScript provider
+
+[TypeScript](http://www.typescriptlang.org/) is a language that extends JavaScript 
+with types. It uses _definition files_ (`*.d.ts`) to specify the types of existing 
+JavaScript libraries. FunScript can import information from TypeScript definition files 
+and generate F# types for accessing the DOM and calling external JavaScript libraries.
+
+The following two lines import definitions for [jQuery](https://github.com/ZachBray/FunScript/blob/master/Examples/Typings/jquery.d.ts)
+and [JavaScript DOM](https://github.com/ZachBray/FunScript/blob/master/Examples/Typings/lib.d.ts) 
+from TypeScript:
+*)
 
 type j = Api<"../Typings/jquery.d.ts">
 type lib = Api<"../Typings/lib.d.ts">
 
-// ----------------------------------------------------------------------------
-// Mini implementation of some F# async primitives
+(**
+Hello world
+*)
 
-type Async =
-  static member AwaitJQueryEvent(f : ('T -> obj) -> j.JQuery) : Async<'T> = 
-    Async.FromContinuations(fun (cont, econt, ccont) ->
-      let named = ref None
-      named := Some (f (fun v -> 
-        (!named).Value.off() |> ignore
-        cont v
-        obj() )))
-      
-// ----------------------------------------------------------------------------
-// Demo using mini F# async
+let hello () = 
+  lib.window.alert("Hello world!")
+
+(**
+## Demo using mini F# async
+*)
 
 let (?) (jq:j.JQueryStatic) name = jq.Invoke("#" + name)
 
-let log(msg:string) =
-   let tag = "<p>" + msg + "</p>"
-   j.jQuery?results.append [| tag :> obj |]
-   |> ignore
-
-let increment(n) = 
-  async {
-    do! Async.Sleep(1000)
-    return n + 1 
-  }
-
-let rec worker(n) = 
-  async { 
-    let! v = Async.AwaitJQueryEvent(fun f -> j.jQuery?next.click(f))
-    let! n = increment(n)
-    do log ("Count: " + n.ToString())
-    return! worker(n)
-  }
-
 let main() = 
-  async {
-    let! x = Async.AwaitJQueryEvent(fun o -> j.jQuery?document.ready o)
-    let cts = new CancellationTokenSource()
-    Async.StartImmediate(worker 0, cts.Token)
-    j.jQuery?stop.click(fun _ -> box <| cts.Cancel()) |> ignore
-  } |> Async.StartImmediate
-
-
-// ----------------------------------------------------------------------------
+  j.jQuery?helloWorld.click(fun _ -> hello())
 
 do Runtime.Run(components=Interop.Components.all)
