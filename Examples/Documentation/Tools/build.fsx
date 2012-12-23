bfd4d2e1f6c56498d401fb7f8c625387e79e27c8
Tomas Petricek <tomas@tomasp.net>
2012-12-23 16:44:38 +0100

    Adding documentation tools

---
+++ Examples/Documentation/Tools/build.fsx
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------
+// Builds the documentation from FSX files in the 'samples' directory
+// (the documentation is stored in the 'docs' directory)
+// --------------------------------------------------------------------------------------
+
+#I "../packages/FSharp.Formatting.1.0.6/lib/net40"
+#load "../packages/FSharp.Formatting.1.0.6/literate/literate.fsx"
+open System.IO
+open FSharp.Literate
+
+let source = __SOURCE_DIRECTORY__
+let template = Path.Combine(source, "template.html")
+let tutorial = Path.Combine(source, "../Tutorial/Page.fs")
+let output = Path.Combine(source, "../Documentation/output/tutorial.html")
+let options = 
+  "-r:\"" + Path.Combine(source, "../../FunJS/bin/Debug/FunJS.dll") + "\" " +
+  "-r:\"" + Path.Combine(source, "../../FunJS.TypeScript/bin/Debug/FunJS.TypeScript.dll") + "\""
+
+Literate.ProcessScriptFile(tutorial, template, output, compilerOptions = options)
---
+++ Examples/Tutorial/Page.fs
@@ -0,0 +1,58 @@
+﻿[<ReflectedDefinition>]
+module Program
+
+open FunJS
+open FunJS.TypeScript
+open System.Threading
+
+type j = FunJS.TypeScript.Api<"..\\Typings\\jquery.d.ts">
+type lib = FunJS.TypeScript.Api<"..\\Typings\\lib.d.ts">
+
+// ----------------------------------------------------------------------------
+// Mini implementation of some F# async primitives
+
+type Async =
+  static member AwaitJQueryEvent(f : ('T -> obj) -> j.JQuery') : Async<'T> = 
+    Async.FromContinuations(fun (cont, econt, ccont) ->
+      let named = ref None
+      named := Some (f (fun v -> 
+        (!named).Value.off'() |> ignore
+        cont v
+        obj() )))
+      
+// ----------------------------------------------------------------------------
+// Demo using mini F# async
+
+let (?) (jq:j.JQueryStatic') name = jq.Invoke'("#" + name)
+
+let log(msg:string) =
+   let tag = "<p>" + msg + "</p>"
+   j.jQuery?results.append [| tag :> obj |]
+   |> ignore
+
+let increment(n) = 
+  async {
+    do! Async.Sleep(1000)
+    return n + 1 
+  }
+
+let rec worker(n) = 
+  async { 
+    let! v = Async.AwaitJQueryEvent(fun f -> j.jQuery?next.click''(f))
+    let! n = increment(n)
+    do log ("Count: " + n.ToString())
+    return! worker(n)
+  }
+
+let main() = 
+  async {
+    let! x = Async.AwaitJQueryEvent(fun o -> j.jQuery?document.ready o)
+    let cts = new CancellationTokenSource()
+    Async.StartImmediate(worker 0, cts.Token)
+    j.jQuery?stop.click''(fun _ -> box <| cts.Cancel()) |> ignore
+  } |> Async.StartImmediate
+
+
+// ----------------------------------------------------------------------------
+
+do Runtime.Run()
