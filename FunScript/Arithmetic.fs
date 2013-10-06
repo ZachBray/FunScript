503425f68f569454c4f6b1ddbaf1ee616bb94baf
Zach Bray <zachbray@googlemail.com>
2013-10-06 21:16:35 +0100

    Added lots of maths functions.

--- FunScript/Arithmetic.fs
+++ FunScript/Arithmetic.fs
@@ -1,6 +1,7 @@
-﻿module internal FunScript.Arithmetic
+﻿module FunScript.Arithmetic
 
 open AST
+open System
 open System.Reflection
 open Microsoft.FSharp.Quotations
 
@@ -11,15 +12,99 @@ let private numericInfix op (mi : MethodBase) lhsT lhs rhsT rhs =
       None
       //Some(Apply(PropertyGet(lhs, mi.Name), [rhs]))
 
+[<JS>]
+type MathJS() =
+    
+    [<JSEmit("return Math.abs({0});")>]
+    static member Abs(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.acos({0});")>]
+    static member Acos(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.asin({0});")>]
+    static member Asin(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.atan({0});")>]
+    static member Atan(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.atan2({0});")>]
+    static member Atan2(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.ceil({0});")>]
+    static member Ceil(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.cos({0});")>]
+    static member Cos(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.exp({0});")>]
+    static member Exp(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.floor({0});")>]
+    static member Floor(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.log({0});")>]
+    static member Log(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.pow({0}, {1});")>]
+    static member Pow(x : float, y : float) : float = failwith "never"
+
+    [<JSEmit("return Math.round({0});")>]
+    static member Round(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.sin({0});")>]
+    static member Sin(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.sqrt({0});")>]
+    static member Sqrt(x : float) : float = failwith "never"
+
+    [<JSEmit("return Math.tan({0});")>]
+    static member Tan(x : float) : float = failwith "never"
+    
+    [<JSEmit("return Math.E;")>]
+    static member E() : float = failwith "never"
+
+    [<JSEmit("return Math.PI;")>]
+    static member PI() : float = failwith "never"
+    
+    [<JSEmit("return Math.LN10;")>]
+    static member LN10() : float = failwith "never"
+
+    static member Log10 x = MathJS.Log x / MathJS.LN10()
+
 let components = [ 
-   CompilerComponent.binaryTyped <@ (+) @> (numericInfix "+")
-   CompilerComponent.binaryTyped <@ (-) @> (numericInfix "-")
-   CompilerComponent.binaryTyped <@ (*) @> (numericInfix "*")
-   CompilerComponent.binaryTyped <@ (/) @> (numericInfix "/")
-   CompilerComponent.binaryTyped <@ (%) @> (numericInfix "%")
-   CompilerComponent.binaryTyped <@ (>>>) @> (numericInfix ">>")
-   CompilerComponent.binaryTyped <@ (<<<) @> (numericInfix "<<")
-   CompilerComponent.binaryTyped <@ (&&&) @> (numericInfix "&")
-   CompilerComponent.binaryTyped <@ (|||) @> (numericInfix "|")
-   CompilerComponent.unaryOp <@ op_UnaryNegation @> "-"
+    yield! [
+        CompilerComponent.binaryTyped <@ (+) @> (numericInfix "+")
+        CompilerComponent.binaryTyped <@ (-) @> (numericInfix "-")
+        CompilerComponent.binaryTyped <@ (*) @> (numericInfix "*")
+        CompilerComponent.binaryTyped <@ (/) @> (numericInfix "/")
+        CompilerComponent.binaryTyped <@ (%) @> (numericInfix "%")
+        CompilerComponent.binaryTyped <@ (>>>) @> (numericInfix ">>")
+        CompilerComponent.binaryTyped <@ (<<<) @> (numericInfix "<<")
+        CompilerComponent.binaryTyped <@ (&&&) @> (numericInfix "&")
+        CompilerComponent.binaryTyped <@ (|||) @> (numericInfix "|")
+        CompilerComponent.unaryOp <@ op_UnaryNegation @> "-"
+
+        ExpressionReplacer.createUnsafe <@ sqrt @> <@ MathJS.Sqrt @>
+        ExpressionReplacer.create <@ fun () -> Math.PI @> <@ fun () -> MathJS.PI() @>
+        ExpressionReplacer.create <@ fun () -> Math.E @> <@ fun () -> MathJS.E() @>
+        ExpressionReplacer.createUnsafe <@ abs @> <@ MathJS.Abs @>
+        ExpressionReplacer.createUnsafe <@ acos @> <@ MathJS.Acos @>
+        ExpressionReplacer.createUnsafe <@ asin @> <@ MathJS.Asin @>
+        ExpressionReplacer.createUnsafe <@ atan @> <@ MathJS.Atan @>
+        ExpressionReplacer.createUnsafe <@ atan2 @> <@ MathJS.Atan2 @>
+        ExpressionReplacer.createUnsafe <@ ceil @> <@ MathJS.Ceil @>
+        ExpressionReplacer.createUnsafe <@ cos @> <@ MathJS.Cos @>
+        ExpressionReplacer.createUnsafe <@ exp @> <@ MathJS.Exp @>
+        ExpressionReplacer.createUnsafe <@ floor @> <@ MathJS.Floor @>
+        ExpressionReplacer.createUnsafe <@ log @> <@ Math.Log @>
+        ExpressionReplacer.createUnsafe <@ log10 @> <@ Math.Log10 @>
+        ExpressionReplacer.createUnsafe <@ pown @> <@ MathJS.Pow @>
+        ExpressionReplacer.createUnsafe <@ round @> <@ MathJS.Round @>
+        ExpressionReplacer.createUnsafe <@ sin @> <@ MathJS.Sin @>
+        ExpressionReplacer.createUnsafe <@ sqrt @> <@ MathJS.Sqrt @>
+        ExpressionReplacer.createUnsafe <@ tan @> <@ MathJS.Tan @>
+    ]
+
+    yield! ExpressionReplacer.createTypeMethodMappings typeof<Math> typeof<MathJS>
+  
 ]
