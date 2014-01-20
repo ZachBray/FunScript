0ce8b9ecb26e667c10fd71d5d756121ddf2b2dda
TalBeno <tal.beno@gmail.com>
2014-01-20 17:39:29 -0500

    Create Program.fs
    The "REAL" first program. Based on the HTML CANVAS example (by ZachBray).
    
    Tested on a vanilla "FunScript - Not Hosted" project (you can find it through the "online" project option).
    
    1. The whole idea is that you create "code". This is where your main() is used - a.k.a. your magic. Here just copied the "canvas" example.
    2. Insert it into an HTML "page". I copied the "canvas" index.html file. And added the script tag into the "body" with reference to the "code" as its content.
    3. The rest is standard - putting the "page" into an index.html file and launching your default browser.
    
    I spent 5 hours on this till I got the hang of it. Maybe will be a 5 minute thing for you now :)

--- Examples/Program.fs
+++ Examples/Program.fs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 (*
 Main module. 
 ============
@@ -23,6 +24,8 @@ Such as the Canvas example (by Zach Bary) - offered as a good first project (hel
 Adding examples for JQueryMobile and PhoneGap (TBD).
 *)
 
+=======
+>>>>>>> Create Program.fs
 [<ReflectedDefinition>]
 module Program
 
@@ -30,6 +33,7 @@ open FunScript
 open FSharp.Data
 open System.IO
 
+<<<<<<< HEAD
 // Each file contains a different example
 open HtmlCanvasExample          // Zach Bary's example, the FS TS code draws 2 rectangles and inject them into the html file that does the rest
 open BodyElementsExample        // Modifying the HtmlCanvasExample to represent TS which populates the whole HTML body
@@ -49,4 +53,57 @@ let filePath = Path.Combine(System.Environment.CurrentDirectory, "index.html")
 File.WriteAllText(filePath, mazingPage)
 
 // Open the file in the default web browser...
+=======
+// Create a function that will be compiled into JavaScript...
+let main () =
+    let canvas = Globals.document.getElementsByTagName_canvas().[0]
+    canvas.width <- 1000.
+    canvas.height <- 800.
+    let ctx = canvas.getContext_2d()
+    ctx.fillStyle <- "rgb(200,0,0)"
+    ctx.fillRect (10., 10., 55., 50.);
+    ctx.fillStyle <- "rgba(0, 0, 200, 0.5)"
+    ctx.fillRect (30., 30., 55., 50.)
+
+// Compile the main() function into javascript code...
+let code =
+    Compiler.Compiler.Compile(
+        // This argument is the quotation to compile
+        <@ main() @>, 
+        // This argument tells the compiler not to wrap 
+        // the result in a return statement
+        noReturn=true//, 
+        // This tells the compiler about the additional components 
+        // that are needed to compile the code. In this case,
+        // these are the components that provide mappings for the
+        // FSharp.Data type providers (incl. the WorldBank provider).
+        //components = FunScript.Data.Components.DataProviders  
+        )
+
+// Here we define the page we'll create...
+// Note: You have to manually include references to the JavaScript dependencies.
+let page = sprintf """<!doctype html>
+<html>
+<head>
+  <script src="http://code.jquery.com/jquery-1.8.0.js"></script>
+  <script type="text/javascript" src="page.js"></script>
+  <meta http-equiv='Content-Type' content='text/html; charset=utf-8'>
+</head>
+<body>
+        <h1><!-- [title] -->Using HTML5 Canvas<!-- [/title] --></h1>
+  
+  <!-- [body] -->
+  <canvas align="center" id="canvas" width="1000" height="800"></canvas>
+  <!-- [/body] -->
+<script>
+%s
+</script>
+</body>
+</html>"""  code
+
+// We write the page to a file...
+let filePath = Path.Combine(System.Environment.CurrentDirectory, "index.html")
+File.WriteAllText(filePath, page)
+// We open the file in the default web browser...
+>>>>>>> Create Program.fs
 System.Diagnostics.Process.Start(filePath) |> ignore
