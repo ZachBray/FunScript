173e8fc33a376d09924a26af681c55bb413e0786
Zach Bray <zachbray@googlemail.com>
2014-10-16 19:10:37 +0100

    Added a more advanced way to add components.

--- src/main/FunScript/Compiler.fs
+++ src/main/FunScript/Compiler.fs
@@ -36,18 +36,24 @@ let private allComponents =
    ] |> List.concat
 
 type Compiler =
-   static member Compile(expression, ?components, ?noReturn, ?shouldCompress) = 
-      let components = defaultArg components []
+   static member private CompileImpl(expression, adjustComponents, noReturn, shouldCompress) = 
       let shouldCompress = defaultArg shouldCompress false
       let returnStrat = 
          if defaultArg noReturn false then ReturnStrategies.inplace
          else ReturnStrategies.returnFrom
-      let compiler = InternalCompiler.Compiler(allComponents @ components)
+      let compiler = InternalCompiler.Compiler(adjustComponents allComponents)
       let program = compiler.Compile returnStrat expression
       let reflectedDefs = compiler.Globals
       let block = List.append reflectedDefs program
       if shouldCompress then (AST.Block block).PrintCompressed()
       else (AST.Block block).Print()
+
+   static member Compile(expression, adjustComponents, ?noReturn, ?shouldCompress) = 
+      Compiler.CompileImpl(expression, adjustComponents, noReturn, shouldCompress)
+
+   static member Compile(expression, ?components, ?noReturn, ?shouldCompress) = 
+      let components = defaultArg components []
+      Compiler.CompileImpl(expression, (fun existingComponents -> existingComponents @ components), noReturn, shouldCompress)
       
 let compile expr = Compiler.Compile(expr)
 let compileWithoutReturn expr = Compiler.Compile(expr, noReturn=true)
