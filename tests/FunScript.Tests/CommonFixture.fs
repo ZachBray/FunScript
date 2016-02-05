f25d715ac982882311edb3dac8c91f1d966866ce
David Podhola <david@podhola.net>
2016-02-05 17:07:45 +0100

    fix #205 by calculating the full path first then trying to remove disk on windows and return to /-styled path at the end

--- tests/FunScript.Tests/CommonFixture.fs
+++ tests/FunScript.Tests/CommonFixture.fs
@@ -4,6 +4,7 @@ module FunScript.Tests.Common
 open FunScript
 open NUnit.Framework
 open Microsoft.FSharp.Linq.QuotationEvaluation
+open System
 open System.IO
 
 let defaultCompile quote =
@@ -68,8 +69,9 @@ let check (quote:Quotations.Expr) =
 let checkRx (quote:Quotations.Expr) =
    let expectedResult = quote.EvalUntyped()
    let universalPath = "/../../paket-files/Reactive-Extensions/RxJS/dist/rx.all.js".Replace('/', Path.DirectorySeparatorChar)
-   printfn "%s" (Path.GetFullPath( sprintf "%s%s" __SOURCE_DIRECTORY__ universalPath ))
-   checkAreEqualWith(sprintf "var Rx = require('%s');" (Path.GetFullPath( sprintf "%s%s" __SOURCE_DIRECTORY__ universalPath )) )
+   let finalPath = Path.GetFullPath( sprintf "%s%s" __SOURCE_DIRECTORY__ universalPath )
+   let rxAllJsPath = "/" + finalPath.Substring(Path.GetPathRoot(finalPath).Length).Replace(Path.DirectorySeparatorChar, '/')
+   checkAreEqualWith(sprintf "var Rx = require('%s');" rxAllJsPath )
         compileWithRx expectedResult quote
 
 let checkAsync (quote:Quotations.Expr<'a Async>) =
