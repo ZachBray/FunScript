2c9b5158fe9199f02828c93d500ed8319584d1dc
David Podhola <david@podhola.net>
2016-02-06 20:22:59 +0100

    make NodeJS sample work again by shamelessly copy-pasting @alfonsogarciacaro code from https://github.com/alfonsogarciacaro/FunScript.Tutorial/blob/master/tutorials/3_NodeServer/Program.fsx

--- examples/NodeJS/Site.fs
+++ examples/NodeJS/Site.fs
@@ -6,28 +6,29 @@ open FunScript
 open FunScript.TypeScript
 open FunScript.Compiler
 
-// Imports
-// Note: this path is relative to the directory you launched mono / monodevelop.
-type ts = Api< @"Examples/Typings/node-0.8.d.ts" >
+[<ReflectedDefinition>]
+module Program =
+    open FunScript.TypeScript
 
-// Server script
-let server() =
-   // Note: magic variable name! The typings file isn't quite correct.
-   // It defines http as a static object rather than a type.
-   let http = unbox<ts.http>(ts.require.Invoke "http")
-   let port = 8124.
-   ts.http
-      .createServer(fun req res ->
-         let stream = res.AsWritableStream()
-         res.writeHead 200.
-         res.``end`` "hello world!")
-      .listen(8124.)
-   ts.console.info [| "listening on port: " + port.ToString() |]
+    // The TypeScript definition define the module methods as static, which create conflicts
+    // with the require statement. To solve this, we create an extension for each requirement.
+    type Node with
+        [<FunScript.JSEmitInline("var http = require('http')")>]
+        static member require_http(): unit = failwith "never"
 
+    let main() =
+        Node.require_http()
+        let port = 8124
 
-// Compile to JS:
-let filePath = "server.js"
-let serverScript = Compiler.Compile(<@ server() @>, components=Interop.Components.all, noReturn=true)
-File.Delete(filePath)
-File.WriteAllText(filePath, serverScript)
-printfn "%s updated!" filePath
+        // Then, to use the module methods we need to open <module>.Globals
+        http.Globals
+            .createServer(System.Func<_,_,_>(fun req res ->
+                res.writeHead 200.
+                res._end "Hello World!"
+            )).listen(port)
+
+// Compile the program and copy the JS code to the parent directory
+open System.IO
+let dir = __SOURCE_DIRECTORY__
+let code = FunScript.Compiler.compileWithoutReturn <@ Program.main() @>
+File.WriteAllText(Path.Combine(dir, "../simplehttpserver.js"), code)
